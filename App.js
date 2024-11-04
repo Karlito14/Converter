52e24e50a1450ba4ae0b373c39c84905da9745f7
Karlito14 <leiroz26@hotmail.com>
2024-11-04 09:46:16 +0100

    mise en plce du layout

--- App.js
+++ App.js
@@ -1,5 +1,17 @@
+import { View, Text, ImageBackground } from 'react-native';
 import { styles } from './App.style';
+import cold from './assets/cold.png';
+import hot from './assets/hot.png';
+
 
 export default function App() {
-  return;
+  return (
+    <ImageBackground source={cold} style={styles.container}>
+      <View style={styles.content}>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+      </View>
+    </ImageBackground>
+  );
 }
--- App.style.js
+++ App.style.js
@@ -1,5 +1,13 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  content: {
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    height: 450
+  },
+});
